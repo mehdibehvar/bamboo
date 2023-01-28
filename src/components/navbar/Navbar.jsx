83bd376158fbc2c43cb1139c17f6f29c40f8ecfd
mehdibehvar <mehdibehvar00@gmail.com>
2023-01-28 10:51:17 +0330

    add-link-register

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,49 +1,52 @@
-import { NavLink } from "react-router-dom"
-import { LogoSvg } from "./LogoSvg"
-import "./Navbar.scss"
+import { NavLink } from "react-router-dom";
+import { LogoSvg } from "./LogoSvg";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-     <div className="navbar_Links_section">
-       <div className="logo_section">
-         <NavLink  to="/">
-         <LogoSvg/>
-         <span className="logo_name">بامبو</span>
-         </NavLink>
-        
-       </div>
+    <nav className="navbar">
+      <div className="navbar_Links_section">
+        <div className="logo_section">
+          <NavLink to="/">
+            <LogoSvg />
+            <span className="logo_name">بامبو</span>
+          </NavLink>
+        </div>
         <ul className="link_list">
-            <li><NavLink
-            to="/courses"
-            className="nav_links"
-          >دوره ها</NavLink></li>
-            <li><NavLink
-            to="/courses"
-            className="nav_links"
-          >مقالات</NavLink></li>
-            <li><NavLink
-            to="/courses"
-            className="nav_links"
-          >خدمات </NavLink></li>
-            <li><NavLink
-            to="/courses"
-            className="nav_links"
-          >درباره ما</NavLink></li>
+          <li>
+            <NavLink to="/courses" className="nav_links">
+              دوره ها
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/courses" className="nav_links">
+              مقالات
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/courses" className="nav_links">
+              خدمات{" "}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/courses" className="nav_links">
+              درباره ما
+            </NavLink>
+          </li>
         </ul>
         <div className="register_section">
-          <NavLink
-            to="login"
-          >ورود</NavLink>
-          <div>
+          <NavLink to="/login">ورود</NavLink>
+          <NavLink to="/register"> <div>
             <button className="register_button"></button>
             <span>ثبت نام</span>
-          </div>
-        </div>
-     </div>
-    <div className='navbar_bottom_border'></div>
-    </nav>
-  )
-}
+          </div></NavLink>
 
-export default Navbar
+         
+        </div>
+      </div>
+      <div className="navbar_bottom_border"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
