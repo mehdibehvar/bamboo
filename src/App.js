a599f77929a6fadaafb5e4bc5770240043ae4067
mehdibehvar <mehdibehvar00@gmail.com>
2023-02-14 16:56:31 +0330

    add-container

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './assets/css/App.scss';
 import Achivments from './components/achivments/Achivments';
 import BannerSection from './components/bannersection/BannerSection';
@@ -7,6 +9,18 @@ import NewsSection from './components/newssection/NewsSection';
 import SuggestionSection from './components/suggestionSection/SuggestionSection';
 
 function App() {
+  useEffect(() => {
+    axios.post('http://localhost:5000/api/auth/register',{
+      "fullName": "mehdi Doe",
+      "email": "mehdi2222@example.com",
+      "password": "12345673",
+      "phoneNumber": "09222222223",
+      "birthDate": "1357/01/26",
+      "nationalId":"6745556259"
+  }).then((res)=>{
+    return res;
+    }).then(res=>console.log(res.data)).catch(err=>console.log(err.response.data))
+  }, [])
   return (
     <div className="App">
       <Layout>
