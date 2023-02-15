ce7f6a0da209a16fefbbec118764c584af2387b5
mehdibehvar <mehdibehvar00@gmail.com>
2023-02-16 00:16:30 +0330

    feat:course-page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import { useEffect, useState } from 'react';
+import { axiosGet, getAllCourses } from './utils/httpclient';
 import './assets/css/App.scss';
 import Achivments from './components/achivments/Achivments';
 import BannerSection from './components/bannersection/BannerSection';
@@ -6,14 +8,23 @@ import CoursesSection from './components/CoursesSection/CoursesSection';
 import Layout from './components/layouts/Layout';
 import NewsSection from './components/newssection/NewsSection';
 import SuggestionSection from './components/suggestionSection/SuggestionSection';
+import axios from 'axios';
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  const getCourses=async ()=>{
+    const response= await getAllCourses();
+   setCourses(response.result)
+  }
+useEffect(() => {
+getCourses();
+}, [])
 
   return (
     <div className="App">
       <Layout>
        <Achivments/>
-       <CoursesSection/>
+       <CoursesSection courses={courses}/>
        <BannerSection/>
        <NewsSection/>
        <SuggestionSection/>
---
+++ src/utils/interceptor/http.interceptor.js
@@ -0,0 +1,35 @@
+import axios, {AxiosResponse } from "axios";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+
+    const expectedError =
+      error.response &&
+      error.response.state >= 400 &&
+      error.response.status < 500;
+    const problem = error
+    if (!expectedError) {
+      return problem
+    }
+    return Promise.reject(error);
+  }
+);
+// axios.interceptors.request.use((config) => {
+//   const token = getItem('token')
+//   config.headers = config.headers || {}
+//   config.headers['x-auth-token'] = JSON.parse(token)
+//   return config
+// })
+
+
+const methods = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+
+export default methods;
